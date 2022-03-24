b56b1cd775d1d5edb31be96a511dfd401963de6d
Maghawry <mag.hauwry@gmail.com>
2022-03-24 23:05:39 +0100

    translation appears now by pressing the button instead of immediatly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import LoginView from './components/views/LoginView.jsx';
 import AppContainer from "./components/hoc/AppContainer.jsx";
+import Translation from "./Translation.jsx";
 
 function App() {
   return (
- 
+    
       <AppContainer>
       <LoginView/>
+      <Translation/>
       </AppContainer>
      
 
--- src/Translation.jsx
+++ src/Translation.jsx
@@ -1,65 +1,71 @@
 import { useState, useEffect } from "react";
-import './css/App.css';
-// import a from './LostInTranslation_Resources/individial_signs/a.png'
-// import b from './LostInTranslation_Resources/individial_signs/b.png'
-// import c from './LostInTranslation_Resources/individial_signs/c.png'
-// import d from './LostInTranslation_Resources/individial_signs/d.png'
-// import e from './LostInTranslation_Resources/individial_signs/e.png'
+import './App.css';
 
 
 const Translation = () => {
-    
-    // const [translation, setTranslation] = useState([]);
     const [translation, setTranslation] = useState('');
-    const letters = translation.split("");
+    const [historyTranslation, addToHisotry]= useState(["Hello!"]);
     const list = [];
-    console.log(letters)
-    // console.log(translation)
-    letters.forEach((letter) => {
-        list.push(<img src={`./LostInTranslation_Resources/individial_signs/${letter}.png`} alt={letter} /> )
-        
-      })
-
+    const historySearch = [];
     
+    historyTranslation.slice(-1)[0].split("").forEach((letter) => {
+            list.push(<img src={`./LostInTranslation_Resources/individial_signs/${letter}.png`} alt="" /> )
+            
+        })
 
+    for (let i = 0; i < 5; i++) {
+        historySearch.push([list])
+        text += "The number is " + i + "<br>";
+        }
+
+// input for translation
     const handleTranslation = (event) =>  {
-        setTranslation(event.target.value);
+        setTranslation(event.target.value)
          }
-
+    
+//    When translation button is pressed
     const handle = () =>  {
-        // <img src={a} alt="" />
-        console.log(translation) 
+        addToHisotry([...historyTranslation, translation])
+        console.log(historyTranslation)
         
     }
-    // 
+
+    useEffect(() => {
+        // console.log('use effect ran');
+    }, [historyTranslation])
 
 
-        return (
+    return (
+        <div>
+            <h1 className="mt-3 mb-1">Translation Page</h1>
+            <form className="mb-3">
+                <fieldset>
+                    <legend> Insert text to translate to sign-language </legend>
+                    <input
+                    type="text"
+                    onChange={ handleTranslation }
+                    className="form-control"
+                    placeholder="E.g. Hello!" />
+                </fieldset>
+            </form>
+            
+            <button button className="btn btn-primary btn-lg mb-4"  
+            onClick={ handle }>Show Translation</button>
             <div>
-                <h1>Translation Page</h1>
-                <form>
-                    <fieldset>
-                        <legend> Insert text to translate to sign-language </legend>
-                        <input
-                        type="text"
-                        onChange={ handleTranslation }
-                        
-                        placeholder="E.g. How are you?" />
-                    </fieldset>
-                </form>
-                
-                <div>
-                    <p>{translation}</p>
-                </div>
-                <button onClick={ handle }>Show Translation</button>
-                <div>
-                    {list}
-                </div>
-                
+                {list}
                 
             </div>
-            );
+            <h6>{historyTranslation.slice(-1)}</h6>
 
-        }
+            <div className="vstack">
+                {historyTranslation}
+            </div>
+            
+            
+            
+        </div>
+        );
+
+    }
 
 export default Translation;
