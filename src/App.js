f19abda89e26b70b499dce3e52a8db0e847f30de
Maghawry <mag.hauwry@gmail.com>
2022-03-25 15:22:16 +0100

    Draft: navbar, links are not set-up

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import LoginView from './components/views/LoginView.jsx';
 import TranslationView from './components/views/TranslationView.jsx';
 import ProfileView from './components/views/ProfileView.jsx';
 import NotFoundView from './components/views/NotFoundView.jsx';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
       <BrowserRouter>
-        <Routes>
-        <Route path="/" element={<LoginView/>}/>
-        <Route path="translation" element={<TranslationView/>}/>
-        <Route path="profile" element={<ProfileView/>}/>
-        <Route path="*" element={<NotFoundView/>}/>
-        </Routes>
+        <Navbar/>
+          <Routes>
+            <Route path="/" element={<LoginView/>}/>
+            <Route path="translation" element={<TranslationView/>}/>
+            <Route path="profile" element={<ProfileView/>}/>
+            <Route path="*" element={<NotFoundView/>}/>
+          </Routes>
       </BrowserRouter>
       
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar mt-3 mb-5">
+            <h1>Sign Language App ✌️</h1>
+            <div className="links">
+                <Link to="/">Login Page</Link>
+                <Link to="/translation">Translation</Link>
+                <Link to="/Profile">Profile History</Link>
+                <Link to="/">Log-Out</Link>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
