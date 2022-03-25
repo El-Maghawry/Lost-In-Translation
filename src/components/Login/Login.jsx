e373451addf120756fc6099a49ba01d2ced990da
iljaasd <iljaasd@gmail.com>
2022-03-25 09:34:40 +0100

    error shown when username is empty

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -9,6 +9,8 @@ const Login = () => {
     const router = useNavigate();
     const session  = useSelector(state => state.session)
     const usernameInput = useRef();
+    
+    const [isEmptyInput, setIsEmptyInput] = useState();
 
     const [credentials, setCredentials] = useState({
         username: ''
@@ -19,12 +21,13 @@ const Login = () => {
             ...credentials,
             [event.target.id]: event.target.value
         })
+        setIsEmptyInput(state => false)
     }
 
     const onFormSubmit = event => {
         event.preventDefault();
         if(usernameInput.current.value === ''){
-            return
+            setIsEmptyInput(state => true)
         }
         else{
             dispatch(loginAttemptAction(credentials.username))
@@ -38,7 +41,7 @@ const Login = () => {
         {!session.loggedIn &&       
         <div>
         <h1 className="header">Welcome to Lost In Translation</h1> 
-        <form className="mt-4" onSubmit={onFormSubmit}>
+        <form className="mt-4 mb-4" onSubmit={onFormSubmit}>
 
             <div className="mb-3">
                 <label htmlFor="username" className="form-label">Username</label>
@@ -46,6 +49,15 @@ const Login = () => {
             </div>
             <button className="btn btn-primary btn-lg" type="submit">Login</button>
         </form>
+        
+        {
+            isEmptyInput && 
+            <div className="alert alert-danger" role="alert">
+                <h4>Username can't be empty</h4>
+                <p>Typ a valid username</p>
+            </div>
+        }
+
         </div>}
         </>
   
